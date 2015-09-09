2a670dca39215212cb28203976e3fbd597874016
amiroslaw <arek47@gmail.com>
2015-09-09 22:24:04 +0200

    tworzenie kilku katalogow z kolecjami, gdy zasobami sa filmy

--- TowrzenieKatalogow/src/Katalogi.java
+++ TowrzenieKatalogow/src/Katalogi.java
@@ -1,9 +1,11 @@
 import java.io.BufferedReader;
-	import java.io.File;
-	import java.io.FileReader;
-	import java.io.IOException;
-	import java.util.ArrayList;
-	import java.util.List;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.text.StyledEditorKit.BoldAction;
 	
 /**
  * pobiera dane z arkusza arkusz, gdzie jest lista quizow
@@ -12,19 +14,29 @@ import java.io.BufferedReader;
  *
  */
 public class Katalogi {
-	
-		// dzieli plik na linie
+	//zmienne
 		static List <String> wiersze= new ArrayList<>(); 
 		static String pathHome = System.getProperty("user.home");
 		static	int liczLinie=0; // ile wierszy
+		static boolean czyFilmy=false; 
+		// dzieli plik na linie
 		public static void czytaj_linie() throws IOException {
-			FileReader fr = new FileReader(pathHome+"/arkusz");
+			FileReader fr = new FileReader(pathHome+"/zestaw");
 			BufferedReader br = new BufferedReader(fr);
 			String d;
+			
 			while ((d = br.readLine()) != null) {
 				//splitCol(d);
 				wiersze.add(d);
 				liczLinie++;
+				if (liczLinie==2) {
+					char pierwszaLitera = wiersze.get(1).charAt(0);
+					if (pierwszaLitera=='P' || pierwszaLitera=='p') {
+						czyFilmy=true; 
+						System.out.println("zasoby filmy");
+					}
+					
+				}
 			}
 
 			br.close();
@@ -39,7 +51,7 @@ public class Katalogi {
 			return slash;
 		}
 
-
+		
 		public static void createFolder(String quizNazwa) {
 
 			//System.out.println(pathHome);
@@ -52,12 +64,21 @@ public class Katalogi {
 			} else {
 				System.out.append("\n Folder utworzony\n");
 			}
-			String folderZip = folderZasobu + "/pliki";
+			
+			// tworzy katalogi na kolecje 4 jesli to filmy
+			int ileKolekcji=1; 
+			if (czyFilmy==true) {
+				ileKolekcji=4; 
+			}
+			for (int i = 1; i <= ileKolekcji; i++) {
+			String folderZip = folderZasobu + "/pliki_"+i;
 			boolean success2 = (new File(folderZip)).mkdirs();
 			if (!success2) {
-				System.out.append("\n Folder plik nie utworzony bo juz istnieje  :\n");
+				System.out.append("\n Folder pliki "+i+" nie utworzony bo juz istnieje  :\n");
 			} else {
-				System.out.append("\n Folder plik utworzony\n");
+				System.out.append("\n Folder pliki "+i+" utworzony\n");
+			}
+				
 			}
 		}
 
